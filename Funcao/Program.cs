9a3afcc828e7604338802716d8559cb87d662be0
Willian Marioti <willian.marioti@gmail.com>
2023-03-05 13:11:55 -0300

    Add project files.

---
+++ Funcao/Program.cs
@@ -0,0 +1,18 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numero, fat;
+
+        Console.WriteLine("Informe o número: ");
+        numero = double.Parse(Console.ReadLine());
+
+
+        for (fat = 1; numero > 1; numero = numero - 1)
+        {
+            fat = fat*numero;
+        }
+        Console.WriteLine(fat);
+
+    }
+}
