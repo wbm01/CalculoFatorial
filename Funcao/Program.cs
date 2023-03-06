81de3061af04fecf8c658dba30b9c011ab909ae5
Willian Marioti <willian.marioti@gmail.com>
2023-03-06 12:00:59 -0300

    Cálculo de fatorial utilizando função.

--- Funcao/Program.cs
+++ Funcao/Program.cs
@@ -2,7 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        double numero, fat;
+        /*double numero, fat;
 
         Console.WriteLine("Informe o número: ");
         numero = double.Parse(Console.ReadLine());
@@ -13,6 +13,27 @@
             fat = fat*numero;
         }
         Console.WriteLine(fat);
+        */
 
+        int num;
+        int resposta;
+
+        Console.WriteLine("Informe um valor: ");
+        num = int.Parse(Console.ReadLine());
+
+        resposta = fat(num);
+        Console.WriteLine("O fatorial de " + num + " é: " + resposta);
+
+        int fat (int valor)
+        {
+            if (valor == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return (valor * fat(valor-1));
+            }
+        }
     }
 }
